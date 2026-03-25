1ce4e66bdc405adddfb540dd79700bf298afdf40
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-03-25 21:54:28 +0530

    Add product API integration: implement CRUD operations and related product retrieval in Redux store

---
+++ frontend/src/redux/features/products/productApi.js
@@ -0,0 +1,82 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { getBaseURL } from "../../../utils/baseURL";
+
+export const productsApi = createApi({
+  reducerPath: "productsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${getBaseURL()}/api/products`,
+    credentials: "include",
+  }),
+  tagTypes: ["Products"],
+
+  endpoints: (builder) => ({
+    // ✅ Get All Products
+    getAllProducts: builder.query({
+      query: ({
+        category = "all",
+        color = "all",
+        minPrice = "",
+        maxPrice = "",
+        page = 1,
+        limit = 10,
+        search = "",
+      }) => ({
+        url: "/",
+        params: {
+          category,
+          color,
+          minPrice,
+          maxPrice,
+          page,
+          limit,
+          search,
+        },
+      }),
+      providesTags: ["Products"],
+    }),
+    getSingleProduct: builder.query({
+      query: (id) => `/${id}`,
+      providesTags: ["Products"],
+    }),
+    createProduct: builder.mutation({
+      query: (newProduct) => ({
+        url: "/create-product",
+        method: "POST",
+        body: newProduct,
+      }),
+      invalidatesTags: ["Products"],
+    }),
+    getRelatedProducts: builder.query({
+      query: (id) => `/related/${id}`,
+      providesTags: ["Products"],
+    }),
+    updateProduct: builder.mutation({
+      query: ({ id, ...data }) => ({
+        url: `/update-product/${id}`,
+        method: "PATCH", // or PATCH (recommended)
+        body: data,
+      }),
+      invalidatesTags: (result, error, { id }) => [
+        "Products",
+        { type: "Products", id },
+      ],
+    }),
+    deleteProduct: builder.mutation({
+      query: (id) => ({
+        url: `/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Products"],
+    }),
+  }),
+});
+
+export const {
+  useGetAllProductsQuery,
+  useGetSingleProductQuery,
+  useCreateProductMutation,
+  useGetRelatedProductsQuery,
+  useDeleteProductMutation,
+  useUpdateProductMutation,
+} = productsApi;
+export default productsApi;
