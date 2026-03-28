a9aa09ee89e80f6c7aafd1a49ae62e0ed389098a
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-03-28 21:12:55 +0530

    Added order model

---
+++ backend/src/orders/orders.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+
+    products: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1
+        }
+      }
+    ],
+
+    totalAmount: {
+      type: Number,
+      required: true
+    },
+
+    orderStatus: {
+      type: String,
+      enum: ["pending", "processing", "shipped", "delivered", "cancelled"],
+      default: "pending"
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+export default Order;
