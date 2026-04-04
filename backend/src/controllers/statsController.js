6a1fc003695d6e39374d2687986dface66bf98df
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-04-04 22:38:54 +0530

    Added admin stats controller and route.

--- backend/src/controllers/statsController.js
+++ backend/src/controllers/statsController.js
@@ -1,6 +1,8 @@
 import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../middleware/asyncHandler.js";
 import Order from "../orders/orders.model.js";
+import Product from "../products/products.model.js";
+import User from "../users/user.model.js";
 import Review from "../reviews/reviews.model.js";
 import mongoose from "mongoose";
 
@@ -42,4 +44,57 @@ const getUserStats = asyncHandler(async (req, res) => {
   );
 });
 
-export { getUserStats };
+const getAdminStats = asyncHandler(async (req, res) => {
+
+  const totalOrders = await Order.countDocuments();
+  const totalProducts = await Product.countDocuments();
+  const totalReviews = await Review.countDocuments();
+  const totalUsers = await User.countDocuments();
+
+  // Total Revenue
+  const totalRevenueResult = await Order.aggregate([
+    {
+      $group: {
+        _id: null,
+        totalRevenue: { $sum: "$totalAmount" },
+      },
+    },
+  ]);
+
+  const totalRevenue =
+    totalRevenueResult.length > 0
+      ? totalRevenueResult[0].totalRevenue
+      : 0;
+
+  // Monthly Revenue
+  const monthlyRevenueResult = await Order.aggregate([
+    {
+      $group: {
+        _id: { $month: "$createdAt" },
+        monthlyRevenue: { $sum: "$totalAmount" },
+      },
+    },
+    {
+      $sort: { "_id": 1 },
+    },
+  ]);
+
+  const monthlyRevenue = monthlyRevenueResult.map((item) => ({
+    month: item._id,
+    revenue: item.monthlyRevenue,
+  }));
+
+  res.status(200).json(
+    new ApiResponse(200, "Admin stats fetched successfully", {
+      totalOrders,
+      totalProducts,
+      totalReviews,
+      totalUsers,
+      totalRevenue,
+      monthlyRevenue,
+    })
+  );
+});
+
+
+export { getUserStats, getAdminStats };
--- backend/src/stats/stats.route.js
+++ backend/src/stats/stats.route.js
@@ -1,11 +1,14 @@
 import express from 'express'
-import { getUserStats } from '../controllers/statsController.js'
+import { getAdminStats, getUserStats } from '../controllers/statsController.js'
 import verifyToken from '../middleware/verifyToken.js'
+import { isAdmin } from '../middleware/admin.middleware.js'
 
 const router = express.Router()
 
-// user stats by email
+// user stats 
 router.get('/user-stats',verifyToken, getUserStats)
 
+// admin stats
+router.get('/admin-stats', verifyToken, isAdmin, getAdminStats )
 
 export default router
