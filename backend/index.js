875f1680c51a626c7c82d9ff0378634a22ba2c5d
vishnu-11124 <vishnusanthosh11124@gmail.com>
2026-04-28 21:20:37 +0530

    Set trust proxy for Express app to enhance security

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,9 @@ import cookieParser from 'cookie-parser';
 const app = express()
 const port = process.env.PORT || 3000;
 
+app.set("trust proxy", 1);
+
+
 // Middleware
 app.use(express.json({ limit: '50mb' }));
 app.use((express.urlencoded({ limit: '50mb' })))
